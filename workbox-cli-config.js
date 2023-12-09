065689f0f0bfaa03111b026d9976d892db729ced
Vladimir <vladimirg@moonsite.co.il>
2023-12-09 15:56:52 +0200

    Initialized workbox

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{ico,html,json,js,css,png,jpg}"
+  ],
+  "swDest": "public/serviceWorkerWorkbox.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
